913639dda66e6f2960a016bd8aa5ac59e8741cfe
Iviwe-Gagayi <119960253+Iviwe-Gagayi@users.noreply.github.com>
2023-01-21 06:49:15 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const hamburgerEl = document.querySelector(".icon");
+
+
+function iconEl() {
+    var x = document.querySelector(".links");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+
+    } else {
+        x.style.display = "block";
+
+    }
+}
